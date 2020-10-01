66d77a73c773dc2cc0698717b4a2160bd088a803
duboc <andersonduboc@gmail.com>
2020-10-01 16:11:32 -0300

    new

---
+++ vsphere/variables.tf
@@ -0,0 +1,11 @@
+variable "vsphere_user" {
+  default = "administrator@vsphere.local"
+}
+
+variable "vsphere_password" {
+  default = "VMware1!"
+}
+
+variable "vsphere_server" {
+  default = "vcsa-01a.corp.tanzu"
+}
